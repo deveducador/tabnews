ed9cf4815b848428202c6c84e8db6bcf91c23a8a
Alex Bessa <deveducador@gmail.com>
2025-12-26 23:24:35 +0000

    feat: update index page content

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 
-const Home = () => <h1>TabNews Clone</h1>;
+const Home = () => {
+  return <>
+    <h1>TabNews</h1>
+    <p>Implementation of a TabNews (tabnews.com.br) clone application.</p>
+  </>
+};
 
 export default Home;
